bc79472ba0c9dfce787f19d5e9cf7158935249be
Mario Sousa <m_santos_sousa@hotmail.com>
2021-03-28 10:19:10 +0100

    Add functions.py

---
+++ functions.py
@@ -0,0 +1,14 @@
+def doSomethingFancy():
+    print("so nice")
+
+def sayHello(name):
+    print("hi "+name)
+
+def printname(*name):
+    print(name)
+
+doSomethingFancy()
+sayHello("mario")
+printname("mario","santos","sousa")
+printname()
+
